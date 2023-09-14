1e4d515f5beb70dff2b544e6d99689e4f5282a3a
Mikkel Hansen <Mikkel.Hansen-CIC@ibm.com>
2023-09-14 17:03:19 +0200

    project init, chatgpt code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bookapp/gobackend
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,177 @@
+package bookapp/gobackend
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Book structure
+type Book struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty"`
+	Title  string             `bson:"title"`
+	Author string             `bson:"author"`
+	PageCount int 			  `bson:"PageCount"`
+}
+
+var client *mongo.Client
+var collection *mongo.Collection
+
+func main() {
+	// MongoDB connection string
+	connectionString := "mongodb://username:password@localhost:27017"
+
+	// Initialize MongoDB client
+	clientOptions := options.Client().ApplyURI(connectionString)
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set a timeout for the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Connect to MongoDB
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get a handle to the "library" database and "books" collection
+	database := client.Database("library")
+	collection = database.Collection("books")
+
+	// Initialize the router
+	router := mux.NewRouter()
+
+	// Define CRUD endpoints
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Retrieve all books from MongoDB
+	cursor, err := collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.Background())
+
+	var books []Book
+	for cursor.Next(context.Background()) {
+		var book Book
+		if err := cursor.Decode(&book); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		books = append(books, book)
+	}
+
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get a single book by ID
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var book Book
+	err = collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&book)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+// Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Insert the new book into MongoDB
+	result, err := collection.InsertOne(context.TODO(), book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	book.ID = result.InsertedID.(primitive.ObjectID)
+	json.NewEncoder(w).Encode(book)
+}
+
+// Update an existing book by ID
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var book Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Update the book in MongoDB
+	_, err = collection.ReplaceOne(context.TODO(), bson.M{"_id": id}, book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	book.ID = id
+	json.NewEncoder(w).Encode(book)
+}
+
+// Delete a book by ID
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	// Delete the book from MongoDB
+	_, err = collection.DeleteOne(context.TODO(), bson.M{"_id": id})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
